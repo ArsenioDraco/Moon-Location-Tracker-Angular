dd9608156dd01361e8930de6e6816127e1a34789
Arseniy Voynov (Arsenio Draco) <voynovarseny@gmail.com>
2026-01-31 16:12:18 +0300

    Update app.ts

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,2 +1,13 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export class App implements OnInit {
+
+  RAD = Math.PI / 180;
+
